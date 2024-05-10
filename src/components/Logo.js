94626a6dc6b84770193576c28c718f1e47671a06
srcho01 <seorim5306@gmail.com>
2024-05-11 05:01:05 +0900

    [Perform] Modify CSS

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import styles from './Logo.module.css'; // 로고의 스타일이 포함된 CSS 파일을 import합니다.
+import styles from './Logo.module.css';
 
 function Logo() {
   return (
     <h1 className={styles.h}>
-      {/* <span>{` `}</span> */}
-      <span className={styles.logo}>너</span>
-      <span>영</span>
-      <span className={styles.logo}>나</span>
-      <span>영</span>
+      <span className={styles.neona}>너</span>
+      <span className={styles.yeong}>영</span>
+      <span className={styles.neona}>나</span>
+      <span className={styles.yeong}>영</span>
     </h1>
   );
 }
