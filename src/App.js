bbc2017c425d2e7cb9649c2f1e483e96b3db612c
srcho01 <seorim5306@gmail.com>
2024-05-13 04:23:01 +0900

    [Feat] Add match/board router

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import {
   useNavigationType,
   useLocation,
 } from "react-router-dom";
+
 import MainLogout from "./pages/MainLogout";
 import Login from "./pages/Login";
-
 import SignUp from "./pages/SignUp";
 import MatchState from "./pages/MatchState";
 import MyPage from "./pages/MyPage";
 import MainLogin from "./pages/MainLogin";
+import MatchBoard from "./pages/MatchBoard";
 
 function App() {
   const action = useNavigationType();
@@ -28,14 +29,15 @@ function App() {
     <Routes>
       <Route path="/" element={<MainLogout />} />
       <Route path="/main/login" element={<MainLogin />} />
-      <Route path="/main/logout" element={<MainLogout />} />
 
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
 
       <Route path="/match/state" element={<MatchState />} />
+      <Route path="/match/board" element={<MatchBoard />} />
 
       <Route path="/mypage" element={<MyPage />} />
+      
     </Routes>
   );
 }
