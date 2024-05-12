f28ad02ea3312009d31c23142fa89fe2e3b62a3b
srcho01 <seorim5306@gmail.com>
2024-05-13 04:22:34 +0900

    [Feat] Dropdown list data

---
+++ src/components/Data.js
@@ -0,0 +1,46 @@
+export const SPORT = [
+    {id: 1, name: '야구', value: 'baseball'},
+    {id: 2, name: '축구', value: 'soccer'},
+    {id: 3, name: 'LoL', value: 'LoL'},
+]
+export const GENDER = [
+    {id: 1, name: '남자', value: '남자'},
+    {id: 2, name: '여자', value: '여자'},
+]
+
+export const TEAM = [
+    {id: 1, name: '야구 - LG 트윈스', value: '야구 - LG 트윈스'},
+    {id: 2, name: '야구 - KT 위즈', value: '야구 - KT 위즈'},
+    {id: 3, name: '야구 - SSG 랜더스', value: '야구 - SSG 랜더스'},
+    {id: 4, name: '야구 - NC 다이노스', value: '야구 - NC 다이노스'},
+    {id: 5, name: '야구 - 두산 베어스', value: '야구 - 두산 베어스'},
+    {id: 6, name: '야구 - KIA 타이거즈', value: '야구 - KIA 타이거즈'},
+    {id: 7, name: '야구 - 롯데 자이언츠', value: '야구 - 롯데 자이언츠'},
+    {id: 8, name: '야구 - 삼성 라이온즈', value: '야구 - 삼성 라이온즈'},
+    {id: 9, name: '야구 - 한화 이글스', value: '야구 - 한화 이글스'},
+    {id: 10, name: '야구 - 키움 히어로즈', value: '야구 - 키움 히어로즈'},
+
+    { id: 11, name: '축구 - 강원', value: '축구 - 강원'},
+    { id: 12, name: '축구 - 광주', value: '축구 - 광주'},
+    { id: 13, name: '축구 - 김천', value: '축구 - 김천'},
+    { id: 14, name: '축구 - 대구', value: '축구 - 대구'},
+    { id: 15, name: '축구 - 대전', value: '축구 - 대전'},
+    { id: 16, name: '축구 - 서울', value: '축구 - 서울'},
+    { id: 17, name: '축구 - 수원FC', value: '축구 - 수원FC'},
+    { id: 18, name: '축구 - 울산', value: '축구 - 울산'},
+    { id: 19, name: '축구 - 인천', value: '축구 - 인천'},
+    { id: 20, name: '축구 - 전북', value: '축구 - 전북'},
+    { id: 21, name: '축구 - 제주', value: '축구 - 제주'},
+    { id: 22, name: '축구 - 포항', value: '축구 - 포항'},
+
+    { id: 23, name: 'LoL - DK', value: 'LoL - DK' },
+    { id: 24, name: 'LoL - DRX', value: 'LoL - DRX' },
+    { id: 25, name: 'LoL - KT', value: 'LoL - KT' },
+    { id: 26, name: 'LoL - OK저축은행', value: 'LoL - OK저축은행' },
+    { id: 27, name: 'LoL - T1', value: 'LoL - T1' },
+    { id: 28, name: 'LoL - 광동', value: 'LoL - 광동' },
+    { id: 29, name: 'LoL - 농심', value: 'LoL - 농심' },
+    { id: 30, name: 'LoL - 젠지', value: 'LoL - 젠지' },
+    { id: 31, name: 'LoL - 피어엑스', value: 'LoL - 피어엑스' },
+    { id: 32, name: 'LoL - 한화생명', value: 'LoL - 한화생명' }
+]
