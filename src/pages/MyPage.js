8a0d9f89b5adab1bfac332f5d5b24f501e75f3bd
srcho01 <seorim5306@gmail.com>
2024-05-13 04:30:26 +0900

    [Feat] Add dropdown list and delete button

--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -1,6 +1,26 @@
 import { useCallback, useState } from "react";
 import { useNavigate } from "react-router-dom";
+
+import styled from "styled-components";
 import styles from "./MyPage.module.css";
+
+const Dropdown = styled.select`
+  padding: 8px;
+  margin-left: 10px;
+  border-radius: 10px;
+  border: 3px solid black;
+  width: 100px;
+  height: 40px;
+  font-size: 16px;
+  color: black;
+`;
+
+const SPORT = [
+  {id: 1, name: '야구', value: '야구'},
+  {id: 2, name: '축구', value: '축구'},
+  {id: 3, name: 'LoL', value: 'LoL'},
+]
+
 
 const MyPage = () => {
   const navigate = useNavigate();
@@ -13,13 +33,25 @@ const MyPage = () => {
     navigate("/main/login");
   }, [navigate]);
 
-  const [sportOpen, setSportOpen] = useState(false);
-  const [teamOpen, setTeamOpen] = useState(false);
-  const toggleSport = () => {
-    setSportOpen(!sportOpen);
+  const onSubmitClick = useCallback(() => {
+    navigate("/main/login");
+  }, [navigate]);
+
+
+  const [selectSport, setSport] = useState([]);
+  const handleSport = e => {
+    const { value } = e.target;
+    if (!selectSport.includes(value)) {
+      setSport([...selectSport, value]);
+    }
   };
-  const toggleTeam = () => {
-    setTeamOpen(!teamOpen);
+
+  const [selectTeam, setTeam] = useState([]);
+  const handleTeam = e => {
+    const { value } = e.target;
+    if (!selectTeam.includes(value)) {
+      setTeam([...selectTeam, value]);
+    }
   };
 
   return (
@@ -73,53 +105,68 @@ const MyPage = () => {
 
             <div className={styles.infoContainer}>
               <b className={styles.infoTitle}> 닉네임 </b>
-              <div className={styles.nickname}>
+              <div className={styles.noneDup}>
                 <input
                   type="text"
-                  className={styles.nicknameInput}
-                  placeholder="물에빠진너구리"
+                  className={styles.noneDupInput}
+                  placeholder="홍길동"
                 />
                 <b className={styles.dupCheck} onClick={onNicknameCheckClick}> 중복확인 </b>
               </div>
             </div>
 
             <div className={styles.infoContainer}>
-              <b className={styles.infoTitle}> 관심 스포츠 </b>
-              <img
-                className={styles.plus}
-                loading="lazy"
-                alt=""
-                src="/plus.svg"
-                onClick={toggleSport}
-              />
-              {sportOpen && (
-                <ul className={styles.list}>
-                  <li>야구</li>
-                  <li>축구</li>
-                  <li>LoL</li>
-                </ul>
-              )}
+              <b className={styles.infoTitle}> 관심 스포츠 (선택) </b>
+              <div className={styles.listContainer}>
+                {selectSport.length === 0 ? '없음' : selectSport.map((value, index) => (
+                  <div key={index} className={styles.list}>
+                    {SPORT.find(sport => sport.value === value)?.name}
+                    <button 
+                      className={styles.delBtn}
+                      onClick={() => setSport(prev => prev.filter(item => item !== value))}>
+                    삭제</button>
+                  </div>
+                ))}
+              </div>
+              <Dropdown value="" onChange={handleSport}>
+                <option value="" disabled>선택</option>
+                {SPORT.map((sport) => (
+                  <option key={sport.id} value={sport.value}>
+                    {sport.name}
+                  </option>
+                ))}
+              </Dropdown>
+
             </div>
 
             <div className={styles.infoContainer}>
-              <b className={styles.infoTitle}> 관심 스포츠팀 </b>
-              <img
-                className={styles.plus}
-                loading="lazy"
-                alt=""
-                src="/plus.svg"
-                onClick={toggleTeam}
-              />
-              {teamOpen && (
-                <ul className={styles.list}>
-                  <li>야구 - LG Twins</li>
-                  <li>야구 - KIA Tigers</li>
-                  <li>축구 - 전북 </li>
-                  <li>LoL - SKT T1</li>
-                </ul>
-              )}
+              <b className={styles.infoTitle}> 관심 스포츠팀 (선택) </b>
+              <div className={styles.listContainer}>
+                {selectTeam.length === 0 ? '없음' : selectTeam.map((value, index) => (
+                  <div key={index} className={styles.list}>
+                    {SPORT.find(sport => sport.value === value)?.name}
+                    <button 
+                      className={styles.delBtn}
+                      onClick={() => setTeam(prev => prev.filter(item => item !== value))}>
+                    삭제</button>
+                  </div>
+                ))}
+              </div>
+              <Dropdown value="" onChange={handleTeam}>
+                <option value="" disabled>선택</option>
+                {SPORT.map((sport) => (
+                  <option key={sport.id} value={sport.value}>
+                    {sport.name}
+                  </option>
+                ))}
+              </Dropdown>
             </div>
           </div>
+
+          <div className={styles.Submit} onClick={onSubmitClick}>
+            수정하기
+          </div>
+
         </div>
       </div>
     </div>
