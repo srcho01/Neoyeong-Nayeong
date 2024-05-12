1d2d06658bc29988cc40ffbef1cbb30abb20a97f
srcho01 <seorim5306@gmail.com>
2024-05-12 17:54:31 +0900

    [Feat] View post modals

---
+++ src/components/popUp/ReadOffline2.js
@@ -0,0 +1,51 @@
+import { useCallback } from "react";
+import styles from "./ReadOffline2.module.css";
+import ProfileCard from "./ProfileCard";
+
+const ReadOffline2 = ({close, allClose}) => {
+  const onBackClick = useCallback(() => {
+    if (close) {
+      close();
+    }
+  }, [close]);
+
+  const onReturnClick = useCallback(() => {
+    if (allClose) {
+      allClose();
+    }
+  }, [allClose]);
+
+  return (
+    <div className={styles.popUpFrame}>
+      <div className={styles.head}>
+        <div className={styles.type}>
+          오프라인
+        </div>
+        <img
+          className={styles.icon}
+          onClick={onReturnClick}
+          loading="lazy"
+          alt=""
+          src="/close-icon.svg"
+        />
+      </div>
+
+      <div className={styles.titleBox}>
+        제목제목 오프라인 제목 제목이 매우 길어요 진짜 길어요 계속 길어요
+      </div>
+
+      <div className={styles.card}> <ProfileCard /> </div>
+
+      <div className={styles.submitContainer}>
+        <div className={styles.Submit} onClick={onBackClick}>
+          이전
+        </div>
+        <div className={styles.Submit} onClick={onReturnClick}>
+          신청
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ReadOffline2;
---
+++ src/components/popUp/ReadOnline1.js
@@ -0,0 +1,97 @@
+import { useState, useCallback } from "react";
+import styles from "./ReadOnline1.module.css";
+import ReadOnline2 from "./ReadOnline2";
+import PortalPopup from "../PortalPopup";
+
+const ReadOnline1 = ({onClose}) => {
+  const onReturnClick = useCallback(() => {
+    if (onClose) {
+      onClose();
+    }
+  }, [onClose]);
+
+  const [isCurrentOpen, setCurrentOpen] = useState(false);
+  const open = useCallback(() => {
+    setCurrentOpen(true);
+  });
+  const close = useCallback(() => {
+    setCurrentOpen(false);
+  });
+
+  const allClose = useCallback(() => {
+    if (onClose) {
+      onClose();
+    }
+    setCurrentOpen(false);
+  }, [onClose]);
+
+  return (
+    <div className={styles.popUpFrame}>
+      <div className={styles.head}>
+        <div className={styles.type}>
+          온라인
+        </div>
+        <img
+          className={styles.icon}
+          onClick={onReturnClick}
+          loading="lazy"
+          alt=""
+          src="/close-icon.svg"
+        />
+      </div>
+
+      <div className={styles.infoContainer}>
+        <b className={styles.infoTitle}>제목</b>
+        <div className={styles.input}>
+          제목제목 온라인 제목 ㅁㄴㄻㄴㅁㄴㅇㄻㄴㅇㄻㄴㅇㄻㄴㅇㅇㄻㄴㅇㄻㄴㅇ
+        </div>
+      </div>
+
+      <div className={styles.infoContainer}>
+        <b className={styles.infoTitle}>모집인원</b>
+        <div className={styles.input}>
+          1
+        </div>
+      </div>
+
+      <div className={styles.articleContainer}>
+        <b className={styles.infoTitle}>하고 싶은 말 (선택) </b>
+        <pre className={styles.article}>
+{`줄바꿈 해줘
+줄바꿈 해줘
+줄바꿈 해줘
+줄바꿈 해줘
+줄바꿈 해줘
+줄바꿈 해줘
+이게 뭐냐
+이거 맞냐
+진짜 뭐 이런 게 다 있냐
+개오바라고 생각한다
+sa
+f
+asd
+f
+a
+`}
+        </pre>
+      </div>
+      
+      <div className={styles.Submit} onClick={open}>
+        다음
+      </div>
+
+      {isCurrentOpen && (
+        <PortalPopup
+          overlayColor="rgba(0, 0, 0, 0)"
+          placement="Centered"
+          onOutsideClick={onReturnClick}
+        >
+          <ReadOnline2 close={close} allClose={allClose}/>
+        </PortalPopup>
+      )}
+
+    </div>
+  );
+};
+
+export default ReadOnline1;
