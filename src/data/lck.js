abd39e7399853dcc6d0740723719a5dd35d912f3
srcho01 <seorim5306@gmail.com>
2024-05-15 01:39:00 +0900

    [Feat] Match local data

---
+++ src/data/lck.js
@@ -0,0 +1,13 @@
+const lol = {
+    "0000": {
+        "team1": "젠지",
+        "team2": "T1",
+        "month": 4,
+        "day": 17,
+        "hour": 14,
+        "minute": 0,
+        "loc": "KSPO DOME"
+    }
+}
+
+export default lol;
