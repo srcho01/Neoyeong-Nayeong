75904059f926ae60c71e39c8f6a9b39a3e849de3
srcho01 <seorim5306@gmail.com>
2024-05-14 16:50:38 +0900

    [Feat] Get schedule information from firestore

--- src/components/MainBody.js
+++ src/components/MainBody.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import { useState, useEffect } from "react";
 import styles from './MainBody.module.css';
+
+import { db } from "../firebase";
+import { getDocs, collection } from "firebase/firestore";
 
 const Schedule = ({team1, team2, month, day, hour, minute, loc}) => {
   return (
@@ -13,7 +17,7 @@ const Schedule = ({team1, team2, month, day, hour, minute, loc}) => {
           {month}월 {day}일
         </div>
         <div className={styles.scheText}>
-          {hour}:{minute}
+          {hour}:{minute < 9 ? `0${minute}` : minute}
         </div>
       </div>
 
@@ -24,29 +28,69 @@ const Schedule = ({team1, team2, month, day, hour, minute, loc}) => {
   );
 };
 
-const schedules = [];
-for (let i = 0; i < 30; i++) {
-  schedules.push(
-    <Schedule 
-        team1="키움"
-        team2="LG인데 만약에 팀명이 진짜 겁나 길면?"
-        month={4}
-        day={18}
-        hour={14}
-        minute={0}
-        loc="사직구장인데 어쩌고 저쩌고 만약에 안사직구장?"
-    />
-  );
+async function getSchedules(sport) {
+  const schedules = [];
+
+  try {
+    const colloecitonRef = collection(db, `Sche${sport}`);
+    const queryResult = await getDocs(colloecitonRef);
+
+    const today = new Date();
+    let count = 0;
+
+    queryResult.forEach((doc) => {
+      if (count >= 10) return; // 최대 10개
+      const data = doc.data();
+      const { month, day } = data;
+
+      // 데이터의 날짜를 Date 객체로 변환
+      const gameDate = new Date(today.getFullYear(), month - 1, day);
+
+      // 오늘 이후의 데이터인 경우 스케줄에 추가
+      if (gameDate >= today || (gameDate.getDate() === today.getDate() && gameDate.getMonth() === today.getMonth())) {
+        schedules.push(data);
+        count++;
+      }
+    });
+
+  } catch (error) {
+    console.error("Error fetching user info:", error);
+  }
+
+  return schedules;
 }
 
-const Component = ({imageSrc}) => {
+const Component = ({imageSrc, sport}) => {
+  const [schedules, setSchedules] = useState([]);
+
+  useEffect(() => {
+    async function fetchSchedules() {
+      const fetchData = await getSchedules(sport);
+      setSchedules(fetchData);
+    }
+
+    fetchSchedules();
+
+    console.log(schedules);
+  }, [sport]);
+
   return (
     <div className={styles.container}>
       <div className={styles.imageContainer}>
         <img src={imageSrc} className={styles.image}/>
       </div>
       <div className={styles.scheContainer}>
-        {schedules}
+        {schedules.map((schedule, index) => (
+          <Schedule 
+            team1={schedule.team1}
+            team2={schedule.team2}
+            month={schedule.month}
+            day={schedule.day}
+            hour={schedule.hour}
+            minute={schedule.minute}
+            loc={schedule.loc}
+          />
+        ))}
       </div>
     </div>
   );
@@ -57,12 +101,15 @@ function MainBody() {
     <div className={styles.frame}>
       <Component
         imageSrc = "/kbo.png"
+        sport = "Baseball"
       />
       <Component
         imageSrc = "/kleague.png"
+        sport = "Soccer"
       />
       <Component
         imageSrc = "/lck.png"
+        sport = "LoL"
       />
     </div>
   );
