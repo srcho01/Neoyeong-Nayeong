551cca5f073a6c476fc6603539b1489c721b837a
srcho01 <seorim5306@gmail.com>
2024-05-15 21:57:47 +0900

    [Perform] Add logo click event if logged in

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import styles from './Logo.module.css';
+import { useNavigate } from "react-router-dom";
 
-function Logo() {
+
+const Logo = ({isLogout}) => {
+  const navigate = useNavigate();
+  const logoClick = useCallback(() => {
+    navigate("/main/login");
+  }, [navigate]);
+
   return (
-    <h1 className={styles.h}>
-      <span className={styles.neona}>너</span>
-      <span className={styles.yeong}>영</span>
-      <span className={styles.neona}>나</span>
-      <span className={styles.yeong}>영</span>
-    </h1>
+    <>
+    {isLogout ? (
+      <h1 className={styles.h}>
+        <span className={styles.neona}>너</span>
+        <span className={styles.yeong}>영</span>
+        <span className={styles.neona}>나</span>
+        <span className={styles.yeong}>영</span>
+      </h1>
+    ) : (
+      <h1 className={styles.h} onClick={logoClick} style={{cursor: "pointer"}}>
+        <span className={styles.neona}>너</span>
+        <span className={styles.yeong}>영</span>
+        <span className={styles.neona}>나</span>
+        <span className={styles.yeong}>영</span>
+      </h1>
+    )}
+  </>
   );
-}
+};
 
 export default Logo;
